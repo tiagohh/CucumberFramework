dca1cfd6cc6b3bf0c217c295156f5d29e51e45c1
TEOA <TEOA@PCSOR242.gft.com>
2018-03-02 16:13:43 -0300

    com chrome driver

--- src/test/java/CucumberFramework/stepFiles/Login.java
+++ src/test/java/CucumberFramework/stepFiles/Login.java
@@ -1,11 +1,26 @@
 package CucumberFramework.stepFiles;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Login {
+	
+	WebDriver driver;
+	
+	@Before
+	public void setup () {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\teoa\\Desktop\\workspace\\CucumberFramework\\src\\test\\java\\CucumberFramework\\resources\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		
+	
+	}
+	
 	@Given("^User navigates to stackoverflow website$")
 	public void user_navigates_to_stackoverflow_website() throws Throwable {
 		System.out.println("Test");
